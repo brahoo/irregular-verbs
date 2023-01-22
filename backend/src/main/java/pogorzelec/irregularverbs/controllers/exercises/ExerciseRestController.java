c9b905e83039655a9eb021c0d4959c8613459218
Piotrek <piotr.pogorzelec@gazeta.pl>
2023-01-22 17:01:42 +0100

    Add REST checking answer and getting next practice.

--- backend/src/main/java/pogorzelec/irregularverbs/controllers/exercises/ExerciseRestController.java
+++ backend/src/main/java/pogorzelec/irregularverbs/controllers/exercises/ExerciseRestController.java
@@ -4,9 +4,8 @@ package pogorzelec.irregularverbs.controllers.exercises;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pogorzelec.irregularverbs.model.exercises.Answer;
 import pogorzelec.irregularverbs.model.exercises.Exercise;
 import pogorzelec.irregularverbs.model.exercises.Exerciser;
 
@@ -18,9 +17,23 @@ public class ExerciseRestController {
     private Exerciser exerciser;
 
     @GetMapping
+    @RequestMapping("/new")
     public ResponseEntity<?> getNew() {
         exerciser.createExercise(5);
         Exercise exercise = exerciser.getExercise();
         return new ResponseEntity<Exercise>(exercise, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?>get() {
+        Exercise exercise = exerciser.getExercise();
+        return new ResponseEntity<Exercise>(exercise, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> checkAnswer(@RequestBody Answer answer) {
+        Boolean response = exerciser.checkAnswerAndSetResult(answer);
+        exerciser.goForward();
+        return new ResponseEntity<Boolean>(response, HttpStatus.OK);
     }
 }
