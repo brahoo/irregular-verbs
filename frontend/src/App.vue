87bf36b6b159deabbb95400285f7fbb85f3be059
Piotrek <piotr.pogorzelec@gazeta.pl>
2023-02-11 14:00:40 +0100

    Add App style.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <h1>Irregular Verbs - Main Page</h1>
+        <h1 id="app-title">IRREGULAR VERBS</h1>
         <a @click="currentView = ''">Main Page</a>|
         <a @click="currentView = 'verbs'">Verbs</a>|
         <a @click="currentView = 'exercises'">Exercises</a>
@@ -29,4 +29,23 @@
 </script>
 
 <style>
+    body {
+        background-color: #000020;
+        color: #EEA400;
+        font-family: 'Lato', sans-serif;
+    }
+
+    #app {
+        margin-left: auto;
+        margin-right: auto;
+        width: 1000px;
+    }   
+
+    #app-title {
+        margin-bottom: 5px;
+        font-size: 44px;
+        font-weight: 900;
+        letter-spacing: 4px;
+        text-align: justify;
+    }
 </style>
