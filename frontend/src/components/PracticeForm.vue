9b7b425d2e35c623660c7de5dc251f939e0743c7
Piotrek <piotr.pogorzelec@gazeta.pl>
2023-03-11 15:01:22 +0100

    PracticForm style.

--- frontend/src/components/PracticeForm.vue
+++ frontend/src/components/PracticeForm.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        <form @submit.prevent="giveAnswer()">
+        <form class="practice-form" @submit.prevent="giveAnswer()">
             <label> {{ verb.firstForm }}</label>
-            <input type="text" v-model="answer.secondForm">
-            <input type="text" v-model="answer.thirdForm">
+            <input class="practice-input" type="text" v-model="answer.secondForm">
+            <input class="practice-input" type="text" v-model="answer.thirdForm">
+            <br/>
             <button>Sprawdź</button>
             <p v-if="error">Podaj obie formy</p>
         </form>
@@ -40,4 +41,53 @@
 </script>
 
 <style>
+    .practice-form {
+        text-align: center;
+        font-size: 30px;
+    }
+
+    .practice-input {
+        color: #EEA400;
+        background-color: #000020;
+        font-size: 30px;
+        width: 200px;
+        padding: 1px;
+        padding-left: 3px;
+        border: 1px solid #EEA400;
+        margin-left: 10px;
+    } 
+
+    .practice-input:focus {
+        outline: none;
+        border-color: #FFC622;
+    }
+
+    .practice-form > button {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 15px;
+        margin-bottom: 5px;
+        width: 110px;
+        padding: 3px;
+        background-color: #EEA400;
+        color: #000020;
+        text-align: center;
+        font-size: 20px;
+        font-weight: 500;
+        letter-spacing: 1px;
+        border: none;
+    }
+
+    .practice-form > button:hover button:focus {
+        outline: none;
+        background-color: #FFC622;
+        color: #222242;
+    }
+
+    .practice-form > button:focus {
+        outline: none;
+        background-color: #FFC622;
+        color: #222242;
+    }
+
 </style>
